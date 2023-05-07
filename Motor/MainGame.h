81bae64a94c2eec0ed2761efae614f3d51e202cd
Jose Santisteban <85416512+JoseSantisteban@users.noreply.github.com>
2023-05-06 20:47:27 -0500

    TA2

--- Motor/MainGame.h
+++ Motor/MainGame.h
@@ -3,13 +3,11 @@
 #include <GL/eglew.h>
 #include "Sprite.h"
 #include "HLSLProgram.h"
-<<<<<<< Updated upstream
-=======
 #include "Window.h"
 #include <vector>
 #include <ctime>
 #include <random>
->>>>>>> Stashed changes
+
 
 enum class GameState {
 	PLAY,EXIT
@@ -26,11 +24,9 @@ private:
 	void processInput();
 	HLSLProgram program;
 	void initShaders();
-<<<<<<< Updated upstream
+
 	float time;
-=======
 	time_t timer; //TA2
->>>>>>> Stashed changes
 
 public:
 	MainGame();
